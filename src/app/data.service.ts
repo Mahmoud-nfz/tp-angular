703850d45b129c8637f7a54efb52f69e8ab71c59
Mahmoud <mahmoudhnefzi@gmail.com>
2023-11-26 00:55:56 +0100

    first commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CV } from './cv.service';
 
 @Injectable({
   providedIn: 'root',
